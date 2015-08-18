538b2f9d68a3cb175f6928ceca48588b475ca8fa
최성원 <lavie.choi@gmail.com>
2015-08-18 12:58:04 +0900

    [추가] 회원가입 서버와 network 연동

--- app/src/main/java/com/nhnnext/android/languageexchange/user/SignUpActivity.java
+++ app/src/main/java/com/nhnnext/android/languageexchange/user/SignUpActivity.java
@@ -4,15 +4,30 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.nhnnext.android.languageexchange.Model.User;
+import com.nhnnext.android.languageexchange.Model.UserParcelable;
 import com.nhnnext.android.languageexchange.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alpha on 2015. 7. 21..
@@ -22,6 +37,8 @@ public class SignUpActivity extends FragmentActivity implements View.OnClickList
     private TextView backLogin;
     private Button requestButton;
     private User userForSignUp;
+    private ProgressDialog progressDialog;
+
     //TODO 가입완료후 자동로그인시 BACK BUTTON 눌러도 회원가입으로 돌아오는 문제 해결(taskAffinity)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,67 +92,105 @@ public class SignUpActivity extends FragmentActivity implements View.OnClickList
                 onBackPressed();
                 break;
             case R.id.sign_up_request_btn:
+
+                //progressBar 표시
+                progressDialog = new ProgressDialog(SignUpActivity.this);
+                progressDialog.setMessage("회원가입 중");
+                progressDialog.show();
+
                 /*
                     서버에 회원가입 요청
                  */
-                new SignUpAsyncTask().execute("target url", userForSignUp);
+                new Thread(new Runnable() {
+                    public void run() {
+                        URL url = null;       // URL 설정
+                        String result = null;
+                        try {
+                            url = new URL("http://10.0.3.2:8080/user");
+                            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                            http.setDefaultUseCaches(false);
+                            http.setDoInput(true);  // 읽기 모드 지정
+                            http.setDoOutput(true); // 쓰기 모드 지정
+                            http.setRequestMethod("POST");  // method POST
+                            //Form tag 방식으로 처리
+                            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+
+                            List<Pair<String, String>> params = new ArrayList<>();
+                            params.add(new Pair<>("userEmail", userForSignUp.getEmail()));
+                            params.add(new Pair<>("userName", userForSignUp.getName()));
+                            params.add(new Pair<>("userPassword", userForSignUp.getPassword()));
+                            params.add(new Pair<>("userGender", userForSignUp.getGender()));
+                            params.add(new Pair<>("userAge", "" + userForSignUp.getAge()));
+                            params.add(new Pair<>("oAuth", userForSignUp.getOauth()));
+
+                            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+                            PrintWriter writer = new PrintWriter(outStream);
+                            writer.write(getQuery(params));
+                            writer.flush();
+                            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR");
+                            BufferedReader reader = new BufferedReader(tmp);
+                            StringBuilder builder = new StringBuilder();
+                            String str;
+                            while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
+                                builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
+                            }
+                            result = builder.toString();
+
+                            /*
+                               회원가입 실패
+                             */
+                            //TODO 실패사유 Toast로 표시
+
+                            /*
+                                회원가입 성공
+                             */
+                            //TODO 추후 자동로그인을 위해 App DB에 회원정보 INSERT, MatchingActivity 호출
+                            if (result.equals("success\n")) {
+                                progressDialog.dismiss();   //progressDialog dismiss
+                                Intent intent = new Intent();
+                                intent.setAction("com.nhnnext.android.action.MATCH");
+                                UserParcelable parcelUser = new UserParcelable(userForSignUp);
+                                intent.putExtra("user", parcelUser);
+                                startActivity(intent);
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
                 break;
-        }
-    }
-
-    private class SignUpAsyncTask extends AsyncTask<Object, Void, Boolean> {
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            //progressBar 표시
-            progressDialog = new ProgressDialog(SignUpActivity.this);
-            progressDialog.setMessage("회원가입 중");
-            progressDialog.show();
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Boolean doInBackground(Object... params) {
-            /*
-                loadXmlFromNetwork 구현부
-                //TODO HttpConnection 구현체 호출, 회원가입 url을 통해 요청
-                //TODO 결과값(성공여부) parsing
-                //TODO 성공시 TRUE return
-                //TODO 실패시 FALSE return
-             */
-            try {
-                Thread.sleep(500); //progressBar 정상 동작 테스트를 위한 sleep, loadXmlFromNetwork구현시 제거
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-            //progressBar 숨기기
-            progressDialog.dismiss();
-
-            /*
-               회원가입 실패
-             */
-            //TODO 실패사유 Toast로 표시
-
-            /*
-                회원가입 성공
-             */
-            //TODO 추후 자동로그인을 위해 App DB에 회원정보 INSERT, MatchingActivity 호출
-            Intent intent = new Intent();
-            intent.setAction("com.nhnnext.android.action.MATCH");
-            startActivity(intent);
         }
     }
 
     protected void enableSignUp(User userForSignUp) {
         requestButton.setVisibility(View.VISIBLE);
         this.userForSignUp = userForSignUp;
+    }
+
+    private String getQuery(List<Pair<String, String>> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (Pair<String, String> pair : params) {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(pair.first, "UTF-8"));
+            result.append("=");
+            if (pair.second == null)
+                result.append(URLEncoder.encode("", "UTF-8"));
+            else
+                result.append(URLEncoder.encode(pair.second, "UTF-8"));
+        }
+        return result.toString();
     }
 }
 
